8214bb83e06b469b3d525390727737171c5537ec
KevinGGGG <index45678@gmail.com>
2017-09-17 15:01:54 +0800

    增加usercontroller註解於登出

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,21 +12,21 @@ class UserController extends Controller
     	return view("login");
     }
     public function postAuthenticate(Request $request){
-    	if(Auth::attempt(['account' => $request->input('account'),'password' => $request->input('password')]))
+    	if(Auth::attempt(['account' => $request->input('account'),'password' => $request->input('password')]))//驗證帳號與密碼
     		return redirect('info');//attemp驗證之密碼為bcrypt後之結果
     	else{
-    		return redirect('/login');
+    		return redirect('/login');//登入失敗則跳回login畫面
         }
 
     }
     public function getInfo(){
     	$user = Auth::user();
-        $advantages = $user->advantages->all();
+        $advantages = $user->advantages->all();//user一對多至advantages
         
     	return view('myinfo',['user' => $user,'advantages' => $advantages]);
     }
     public function getLogout(){
-    	Auth::logout();
+    	Auth::logout();//登出使用者帳號
     	return redirect('login');
     }
 
